6e0fb52e5661c13c5994b4c8196550d03f4c523e
samueljs <jssamuel@iesrafaelalberti.es>
2024-09-27 19:41:04 +0200

    actividades v1 ej01 al ej27

---
+++ src/ej05.py
@@ -0,0 +1,3 @@
+Sin_IVA =  int(input("Escribe el  importe sin iva : "))
+IVA = (Sin_IVA*1.21)
+print(f"precio final: {IVA}")
---
+++ src/ej06.py
@@ -0,0 +1,3 @@
+IVA = float (input("Escribe el  importe con iva : "))
+Sin_IVA = (IVA / 1.21)
+print(f"precio final: {Sin_IVA}")
